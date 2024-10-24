6ec105c02aca70164ad4f44d3532ab5f7510c5ed
MarilaMorales <mmorales@fwdcostarica.com>
2024-10-24 13:18:51 -0600

    Views

--- Receta_cocina/api/urls.py
+++ Receta_cocina/api/urls.py
@@ -2,13 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.ProductoListCreate.as_view(), name='producto-list'),
-    path('productos/<int:pk>/', views.ProductoDetail.as_view(), name='producto-detail'),
-    path('cliente/', views.ClienteListCreate.as_view(), name='cliente-list'),
-    path('cliente/<int:pk>/', views.ClienteDetail.as_view(), name='cliente-detail'),
-    path('pedido/', views.PedidoListCreate.as_view(), name='pedido-list'),
-    path('pedido/<int:pk>/', views.PedidoDetail.as_view(), name='pedido-detail'),
-    path('venta/', views.VentaListCreate.as_view(), name='venta-list'),
-    path('venta/<int:pk>/', views.VentaDetail.as_view(), name='venta-detail'),
+    path('recetas/', views.RecetasListCreate.as_view(), name='receta-list'),
+    path('recetas/<int:pk>/', views.RecetasDetail.as_view(), name='producto-detail'),
     
 ]
