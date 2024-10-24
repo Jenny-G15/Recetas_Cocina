f644e87d4536eaa7dde38dadac318d9a4c4be3fb
Jenny-G15 <jguadamuz@fwdcostarica.com>
2024-10-24 13:25:57 -0600

    cambio

--- Receta_cocina/api/urls.py
+++ Receta_cocina/api/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('productos/', views.ProductoListCreate.as_view(), name='producto-list'),
-    path('productos/<int:pk>/', views.ProductoDetail.as_view(), name='producto-detail'),
-    path('cliente/', views.ClienteListCreate.as_view(), name='cliente-list'),
-    path('cliente/<int:pk>/', views.ClienteDetail.as_view(), name='cliente-detail'),
-    path('pedido/', views.PedidoListCreate.as_view(), name='pedido-list'),
-    path('pedido/<int:pk>/', views.PedidoDetail.as_view(), name='pedido-detail'),
-    path('venta/', views.VentaListCreate.as_view(), name='venta-list'),
-    path('venta/<int:pk>/', views.VentaDetail.as_view(), name='venta-detail'),
+    path('recetas/', views.RecetasListCreate.as_view(), name='recetas-list'),
+    path('recetas/<int:pk>/', views.RecetasDetail.as_view(), name='recetas-detail'),
+    path('recetaIngre/', views.RecetaIngreListCreate.as_view(), name='recetaIngre-list'),
+    path('recetaIngre/<int:pk>/', views.RecetaIngreDetail.as_view(), name='recetaIngre-detail'),
+    path('ingredientes/', views.IngredientesListCreate.as_view(), name='ingredientes-list'),
+    path('ingredientes/<int:pk>/', views.IngredientesDetail.as_view(), name='ingredientes-detail'),
+    path('categoria/', views.CategoriaListCreate.as_view(), name='categoria-list'),
+    path('categoria/<int:pk>/', views.CategoriaDetail.as_view(), name='categoria-detail'),
+    path('preparacion/', views.PreparacionListCreate.as_view(), name='preparacion-list'),
+    path('preparacion/<int:pk>/', views.PreparacionDetail.as_view(), name='preparacion-detail'),
+    path('proveedor/', views.ProveedorListCreate.as_view(), name='proveedor-list'),
+    path('proveedor/<int:pk>/', views.ProveedorDetail.as_view(), name='proveedor-detail'),
     
 ]
